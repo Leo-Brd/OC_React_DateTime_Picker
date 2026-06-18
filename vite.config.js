2377a83b4e5e72f77155cf8502ad207d563efea9
l.bordet <leobordet.pro@gmail.com>
2026-06-18 16:30:21 +0200

    fix: config dont work with commonjs

--- vite.config.js
+++ vite.config.js
@@ -1,33 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.js'),
-      name: 'ReactDateTimePicker'
+      entry: 'src/index.js',
+      name: 'ReactDateTimePicker',
+      fileName: (format) => {
+        if (format === 'es') return 'index.esm.js'
+        return 'index.umd.js'
+      }
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
-      output: [
-        {
-          format: 'es',
-          entryFileNames: 'index.esm.js',
-          dir: 'dist'
-        },
-        {
-          format: 'umd',
-          name: 'ReactDateTimePicker',
-          entryFileNames: 'index.umd.js',
-          globals: {
-            react: 'React',
-            'react-dom': 'ReactDOM'
-          },
-          dir: 'dist'
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
         }
-      ]
+      }
     }
   }
 })
